a8a185b4ab0057c29954edc76f705a8e459d7e6f
killerbobjr <github@stinkycats.net>
2022-06-18 21:03:55 -0700

    Changes default 404 and 500 pages to use Sakura.css

--- index.js
+++ index.js
@@ -695,46 +695,10 @@ Keystone.prototype.mount = function(mountPath, parentApp, events) {
 			});
 		}
 
-		// Handle 403 (no route matched) errors
-		
-		var default403Handler = function(req, res, next) {
-			res.status(403).send(keystone.wrapHTMLError("Sorry, no page could be found at this address (403)"));
-		};
-		
-		app.use(function(req, res, next) {
-			
-			var err403 = keystone.get('403');
-			
-			if (err403) {
-				try {
-					if ('function' === typeof err403) {
-						err403(req, res, next);
-					} else if ('string' === typeof err403) {
-						res.status(403).render(err403);
-					} else {
-						if (keystone.get('logger')) {
-							console.log(dashes + 'Error handling 403 (not found): Invalid type (' + (typeof err403) + ') for 403 setting.' + dashes);
-						}
-						default403Handler(req, res, next);
-					}
-				} catch(e) {
-					if (keystone.get('logger')) {
-						console.log(dashes + 'Error handling 403 (not found):');
-						console.log(e);
-						console.log(dashes);
-					}
-					default403Handler(req, res, next);
-				}
-			} else {
-				default403Handler(req, res, next);
-			}
-			
-		});
-		
 		// Handle 404 (no route matched) errors
 		
 		var default404Handler = function(req, res, next) {
-			res.status(404).send(keystone.wrapHTMLError("Sorry, no page could be found at this address (404)"));
+			res.status(404).send(keystone.wrapHTMLError("No page could be found at this address (404)"));
 		};
 		
 		app.use(function(req, res, next) {
@@ -796,7 +760,7 @@ Keystone.prototype.mount = function(mountPath, parentApp, events) {
 				}
 			}
 			
-			res.status(500).send(keystone.wrapHTMLError("Sorry, an error occurred loading the page (500)", msg));
+			res.status(500).send(keystone.wrapHTMLError("The server encountered an error (500)", msg));
 		};
 		
 		app.use(function(err, req, res, next) {
@@ -1778,8 +1742,8 @@ Keystone.prototype.populateRelated = function(docs, relationships, callback) {
 
 Keystone.prototype.wrapHTMLError = function(title, err) {
 	return "<html><head><meta charset='utf-8'><title>Error</title>" +
-	"<link rel='stylesheet' href='/assets/css/error.css'>" +
-	"</head><body><div class='error'><h1 class='error-title'>" + title + "</h1>" + "<div class='error-message'>" + (err || '') + "</div></div></body></html>";
+	"<link rel='stylesheet' href='https://unpkg.com/sakura.css/css/sakura.css'>" +
+	"</head><body><div class='error' style='text-align:center;padding-top:10em;'><h1 class='error-title'>" + title + "</h1>" + "<div class='error-message'>" + (err || '') + "</div></div></body></html>";
 };
 
 /**
